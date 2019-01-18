7793cb853df750ed01b5e6711747fb9544041a56
Leo Toikka <leo@seravo.fi>
2019-01-18 22:58:16 +0200

    Create a HTML table with semantic-ui that conatains most active stocks

--- ui/app/src/App.js
+++ ui/app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://localhost:8080/api/v1/last')
+    fetch('/api/v1/mostactive')
       .then(res => res.json())
       .then(
         (result) => {
@@ -41,13 +42,38 @@ class App extends Component {
       return <div>Loading...</div>;
     } else {
       return (
-        <ul>
+        <Table className="most-active-table" celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Symbol</Table.HeaderCell>
+              <Table.HeaderCell>Change</Table.HeaderCell>
+              <Table.HeaderCell>Low</Table.HeaderCell>
+              <Table.HeaderCell>High</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
           {items.map(item => (
-            <li key={item.symbol}>
-              {item.symbol} {item.price}
-            </li>
+            <Table.Row key={item.symbol}>
+              <Table.Cell>
+                {item.companyName}
+              </Table.Cell>
+              <Table.Cell>
+                {item.symbol}
+              </Table.Cell>
+              <Table.Cell className={item.change < 0 ? 'change-negative' : 'change-positive'}>
+                {item.change > 0 ? '+' + item.change : item.change}
+              </Table.Cell>
+              <Table.Cell>
+                {item.low}
+              </Table.Cell>
+              <Table.Cell>
+                {item.high}
+              </Table.Cell>
+            </Table.Row>
           ))}
-        </ul>
+          </Table.Body>
+        </Table>
       );
     }
   }
