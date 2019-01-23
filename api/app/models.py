344f812a3f732d408cc6d8502b2e139b82517ac5
Leo Toikka <leotoikka1@gmail.com>
2019-01-23 22:56:51 +0200

    Add the user email to the database model

--- api/app/models.py
+++ api/app/models.py
@@ -4,10 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=False, nullable=False)
     password = db.Column(db.String(200), unique=False, nullable=False)
 
     def __init__(self, username, password):
