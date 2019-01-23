6547b2fa9a57c245cba9e495f0b4a4b4a8305290
Leo Toikka <leotoikka1@gmail.com>
2019-01-23 22:53:53 +0200

    Provide mysql host as environment variable

--- api/app/settings.py
+++ api/app/settings.py
@@ -5,6 +5,8 @@ db_file = tempfile.NamedTemporaryFile()
 mysql_user = os.environ['MYSQL_USER']
 mysql_pw = os.environ['MYSQL_PASSWORD']
 mysql_db = os.environ['MYSQL_DATABASE']
+mysql_host = os.environ['MYSQL_DATABASE_HOST']
+
 
 class Config(object):
     SECRET_KEY = 'REPLACE ME'
@@ -12,7 +14,7 @@ class Config(object):
 
 class ProdConfig(Config):
     ENV = 'prod'
-    SQLALCHEMY_DATABASE_URI = f'mysql://{mysql_user}:{mysql_pw}@db:3306/{mysql_db}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{mysql_user}:{mysql_pw}@{mysql_host}:3306/{mysql_db}'
 
     CACHE_TYPE = 'simple'
 
@@ -22,7 +24,7 @@ class DevConfig(Config):
     DEBUG = True
     DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-    SQLALCHEMY_DATABASE_URI = f'mysql://{mysql_user}:{mysql_pw}@db:3306/{mysql_db}'
+    SQLALCHEMY_DATABASE_URI = f'mysql://{mysql_user}:{mysql_pw}@{mysql_host}:3306/{mysql_db}'
 
     CACHE_TYPE = 'null'
     ASSETS_DEBUG = True
