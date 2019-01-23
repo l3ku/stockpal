dd2fc571c3340f99f7881c75c525c29519235a82
Leo Toikka <leotoikka1@gmail.com>
2019-01-23 22:56:17 +0200

    Modify user DB schema to include string length as required by dialect

--- api/app/models.py
+++ api/app/models.py
@@ -7,8 +7,8 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String())
-    password = db.Column(db.String())
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(200), unique=False, nullable=False)
 
     def __init__(self, username, password):
         self.username = username
