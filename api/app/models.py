2e6c9d924dd74d12c4c4d2a62b23bf174750e88d
Leo Toikka <leotoikka1@gmail.com>
2019-01-24 01:31:40 +0200

    Add support for user-specific OAuth2 tokens into the database

--- api/app/models.py
+++ api/app/models.py
@@ -21,3 +21,20 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+class OAuth2Token(db.Model):
+    user_id = db.Column(db.Integer(), db.ForeignKey(User.id), primary_key=True, nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+
+    token_type = db.Column(db.String(20))
+    access_token = db.Column(db.String(48), nullable=False)
+    refresh_token = db.Column(db.String(48))
+    expires_at = db.Column(db.Integer(), default=0)
+
+    def to_token(self):
+        return dict(
+            access_token=self.access_token,
+            token_type=self.token_type,
+            refresh_token=self.refresh_token,
+            expires_at=self.expires_at,
+        )
