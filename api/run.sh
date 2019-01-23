29acfb963b8542cbbe41f31a590924fbaf720e85
Leo Toikka <leotoikka1@gmail.com>
2019-01-23 22:58:24 +0200

    Perform DB model migrations during container startup in entrypoint

--- api/run.sh
+++ api/run.sh
@@ -9,4 +9,11 @@ while ! mysqladmin ping -h"$MYSQL_DATABASE_HOST" --silent; do
   echo "Waiting for database... (If this gets stuck, check that MYSQL_DATABASE_HOST is set and the DB is up and running)"
   sleep 3
 done
+
+# Run database upgrades
+flask db upgrade -d ${APPDIR}/app/migrations
+flask db migrate -d ${APPDIR}/app/migrations
+flask db upgrade -d ${APPDIR}/app/migrations
+
+# Start uwsgi
 uwsgi --ini ${APPDIR}/uwsgi.ini
