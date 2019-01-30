569ca308fd69debe1b2b4164d311a835a1a5d329
Leo Toikka <leotoikka1@gmail.com>
2019-01-30 15:52:01 +0200

    Add a function for retreiving user data with an access token

--- api/app/auth.py
+++ api/app/auth.py
@@ -1,6 +1,7 @@
 import os
 from authlib.client import OAuth2Session
 from flask import request, session
+import requests
 
 def getOAuth2ProviderData(auth_provider):
     oauth2_providers = {
@@ -36,3 +37,16 @@ def OAuth2Login(auth_provider, auth_response):
     # TODO: save this token to the database for access
     token = oauth_session.fetch_access_token(provider_data['access_token_url'], authorization_response=auth_response)
     return True
+def getUserInfoWithAcessToken(auth_provider, auth_token):
+    name=email=picture=None
+    # Handle different providers
+    if auth_provider == 'google':
+        payload = {'alt': 'json', 'access_token': auth_token}
+        response = requests.get('https://www.googleapis.com/oauth2/v1/userinfo', params=payload).json()
+        name = response['name']
+        email = response['email']
+        picture = response['picture']
+    else:
+        raise ValueError(f'Unknown OAuth provider: {auth_provider}')
+
+    return (name, email, picture)
