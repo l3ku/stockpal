1a3043608a7f2923921f2a6ed986ef9bed491e4f
Leo Toikka <leotoikka1@gmail.com>
2019-01-30 15:54:37 +0200

    Update database model to reflect previous changes

--- api/app/models.py
+++ api/app/models.py
@@ -3,15 +3,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class LoggedInUser(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(100), unique=False, nullable=False)
-    password = db.Column(db.String(200), unique=False, nullable=False)
+    login_id = db.Column(db.String(200), unique=True, nullable=False)
+    login_secret = db.Column(db.String(1000), unique=False, nullable=False)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    picture_url = db.Column(db.String(200), unique=False, nullable=True)
 
-    def __init__(self, username, password):
-        self.username = username
-        self.set_password(password)
+    def __init__(self, login_id, login_secret, name, email, picture_url):
+        self.login_id = login_id
+        self.login_secret = login_secret
+        self.name = name
+        self.email = email
+        self.picture_url = picture_url
 
     def is_active(self):
         return True
@@ -23,18 +28,15 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 class OAuth2Token(db.Model):
-    user_id = db.Column(db.Integer(), db.ForeignKey(User.id), primary_key=True, nullable=False)
-    name = db.Column(db.String(20), nullable=False)
-
-    token_type = db.Column(db.String(20))
-    access_token = db.Column(db.String(48), nullable=False)
-    refresh_token = db.Column(db.String(48))
+    user_id = db.Column(db.Integer(), db.ForeignKey(LoggedInUser.id), primary_key=True, nullable=False)
+    token_type = db.Column(db.String(2000))
+    access_token = db.Column(db.String(1000), nullable=False)
+    refresh_token = db.Column(db.String(1000))
     expires_at = db.Column(db.Integer(), default=0)
 
-    def to_token(self):
-        return dict(
-            access_token=self.access_token,
-            token_type=self.token_type,
-            refresh_token=self.refresh_token,
-            expires_at=self.expires_at,
-        )
+    def __init__(self, user_id, token_type, access_token, refresh_token, expires_at):
+        self.user_id = user_id
+        self.token_type = token_type
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.expires_at = expires_at
