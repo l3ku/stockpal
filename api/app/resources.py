18a177092d342cdcbd9548ac40123c0522d792e8
Leo Toikka <leotoikka1@gmail.com>
2019-02-07 00:09:30 +0200

    Get all stocks listing from local JSON to speed up requests

--- api/app/resources.py
+++ api/app/resources.py
@@ -1,12 +1,14 @@
 import re
 import json
 import time
+import json
+import os
 from flask import request
 from flask_restful import Resource, reqparse
 import requests
 from app.auth import initOAuth2Session, OAuth2Login, logout
 from authlib.common.errors import AuthlibBaseError
-from app.models import *
+from app.models import db, AppMetaData, Stock, LoggedInUser, User
 
 iex_api_url = 'https://api.iextrading.com/1.0'
 
@@ -17,50 +19,33 @@ class ListGainers(Resource):
 
 class AllStocks(Resource):
     def get(self):
-        # Update the stocks listing every hour from IEX upstream
-        db_last_updated_meta_data = AppMetaData.query.filter_by(type='all_stocks_update_time').first()
-        if db_last_updated_meta_data is None:
-            stock_data = getAndUpdateStocksFromAPI()
-            # Insert the last updation meta data and commit to these changes.
-            db_last_updated_meta_data = AppMetaData('all_stocks_update_time', str(time.time()))
-            db.session.commit()
-        else:
-            stock_data = Stock.query.all()
-            updation_time = int(db_last_updated_meta_data.value)
-            one_hour_ago = time.time() - 3600
-            if updation_time <= one_hour_ago or all_stocks is None:
-                stock_data = self.getAndUpdateStocksFromAPI()
-                db.session.commit()
-            return stock_data
-
-    def getAndUpdateStocksFromAPI():
-        return_val = []
-        response = requests.get(iex_api_url + '/ref-data/symbols')
-        all_stocks = response.json()
-        for stock in all_stocks:
-            db_stock = Stock.query.filter_by(symbol=stock['symbol']).first()
-            if db_stock is None:
-                db_stock = Stock(symbol=stock['symbol'], name=stock['name'], is_enabled=stock['isEnabled'])
-            else:
-                db_stock.symbol = stock['symbol']
-                db_stock.name = stock['name']
-                db_stock.is_enabled = stock['isEnabled']
-            return_val.append({'symbol': stock['symbol'], 'name': stock['name'], 'is_enabled': stock['isEnabled']})
-        return return_val
+        # Get the stocks from a local file because making the API request every time would be
+        # painfully slow...
+        stocks_file = os.environ['APPDIR'] + '/app/json/all-stocks.json'
+        if os.path.isfile(stocks_file):
+            try:
+                with open(stocks_file, 'r') as f:
+                    stock_data = json.load(f)
+                    return {'success': True, 'data': stock_data}
+            except ValueError:
+                pass
+        return {'success': False, 'error': ''}
 
 
 class UserInfo(Resource):
     def get(self, login_id):
-        if login_id is None or 'X-API-Key' not in request.headers:
-            return {'success': False, 'error': {'reason': 'missing_parameter'}}
+        if login_id is None:
+            return {'success': False, 'error': {'reason': 'missing_parameter', 'target': 'api_id'}}
+        if 'X-API-Key' not in request.headers:
+            return {'success': False, 'error': {'reason': 'missing_header', 'target': 'X-API-Key'}}
         login_secret = request.headers['X-API-Key']
         try:
             db_logged_in_user = LoggedInUser.query.filter_by(login_id=login_id).first()
             if db_logged_in_user is None:
-                return {'success': False, 'error': {'reason': 'invalid_login'}}
+                return {'success': False, 'error': {'reason': 'invalid_login', 'target': None}}
             success, response = db_logged_in_user.validateLogin(login_secret)
             if not success:
-                return {'success': False, 'error': {'reason': response}}
+                return {'success': False, 'error': {'reason': response, 'target': None}}
             db_user = response
             return {'success': True, 'data': {'user_name': db_user.name, 'user_email': db_user.email, 'user_picture_url': db_user.picture_url}}
         except ValueError as err:
@@ -98,7 +83,7 @@ class Logout(Resource):
         args = parser.parse_args()
         login_id = args['api_id']
         if not 'X-API-Key' in request.headers:
-            return {'success': False, 'errors': ['Access denied: X-API-Key is missing']}
+            return {'success': False, 'error': {'reason': 'missing_header', 'target': 'X-API-Key'}}
         login_secret = request.headers['X-API-Key']
         try:
             return {'success': logout(login_id, login_secret)}
