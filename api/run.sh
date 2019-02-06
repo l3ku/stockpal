f1ce992604aeeb87e555d5f7eb9162dd3db8e1be
Leo Toikka <leotoikka1@gmail.com>
2019-02-07 00:05:35 +0200

    Remove virtualenv as Docker is already sufficiently isolated

--- api/run.sh
+++ api/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Use virtual env
-. ${APPDIR}/venv/bin/activate
-
 # Wait for database
 while ! mysqladmin ping -h"$MYSQL_DATABASE_HOST" --silent; do
   echo "Waiting for database... (If this gets stuck, check that MYSQL_DATABASE_HOST is set and the DB is up and running)"
