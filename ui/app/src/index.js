e638090d480673e4861a586489341628f36d4109
Leo Toikka <leotoikka1@gmail.com>
2019-02-20 14:55:14 +0200

    Remove Root.jsx, directly create root component in index.js

--- ui/app/src/index.js
+++ ui/app/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './dist/index.css';
-import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import { CookiesProvider } from 'react-cookie';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const root = (
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>
+);
+
+ReactDOM.render(root, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
