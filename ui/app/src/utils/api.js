23edc62155eafc667ade454a386a9e29b62cfe54
Leo Toikka <leotoikka1@gmail.com>
2019-03-06 21:23:01 +0200

    Remove unnecessary methods from the API that have been moved to actions

--- ui/app/src/utils/api.js
+++ ui/app/src/utils/api.js
@@ -1,22 +1,6 @@
 export default class API {
-  static getGainerStocks(successCallback, errorCallback) {
-    fetch('/api/v1/gainers')
-      .then(res => res.json())
-      .then((res) => successCallback(res), (err) => errorCallback(err));
-  }
-
   static getStockChart(symbol, successCallback, errorCallback) {
     fetch('/api/v1/stock/' + encodeURIComponent(symbol) + '/chart')
-      .then(res => res.json())
-      .then((res) => successCallback(res), (err) => errorCallback(err));
-  }
-
-  static getStockInfo(symbol, successCallback, errorCallback) {
-    let query = '/api/v1/stock';
-    if ( symbol ) {
-      query += '/' + encodeURIComponent(symbol);
-    }
-    fetch(query)
       .then(res => res.json())
       .then((res) => successCallback(res), (err) => errorCallback(err));
   }
@@ -29,29 +13,15 @@ export default class API {
       .then((res) => successCallback(res), (err) => errorCallback(err));
   }
 
-  static getLoginAuthLink(provider, successCallback, errorCallback) {
-    fetch('/api/oauth/authenticate/' + encodeURIComponent(provider))
-      .then(res => res.json())
-      .then((res) => successCallback(res), (err) => errorCallback(err));
-  }
-
-  static sendLoginAuthResponse(provider, authResponse, successCallback, errorCallback) {
-    fetch('/api/oauth/login/' + escape(provider), {
+  static addUserStock(symbol, apiID, apiSecret, successCallback, errorCallback) {
+    fetch('/api/protected/stocks/' + encodeURIComponent(apiID), {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': apiSecret
+      },
       body: JSON.stringify({
-        'authorization_response': authResponse
-      })})
-      .then(res => res.json())
-      .then((res) => successCallback(res), (err) => errorCallback(err));
-  }
-
-  static logout(api_id, api_secret, successCallback, errorCallback) {
-    fetch('/api/oauth/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'X-API-Key': api_secret },
-      body: JSON.stringify({
-        'api_id': api_id
+        'stock_symbol': symbol
       })})
       .then(res => res.json())
       .then((res) => successCallback(res), (err) => errorCallback(err));
