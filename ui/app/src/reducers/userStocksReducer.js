472abf97c2480a2755a4e4da102fc1af44c5ec0f
Leo Toikka <leotoikka1@gmail.com>
2019-03-12 14:09:05 +0200

    Bugfix: always include previous state in reducer reponse

--- ui/app/src/reducers/userStocksReducer.js
+++ ui/app/src/reducers/userStocksReducer.js
@@ -30,11 +30,13 @@ export default function(state=initialState, action) {
       };
     case RECEIVE_ADD_USER_STOCK:
       return {
+        ...state,
         success: true,
         symbol: action.symbol
       };
     case RECEIVE_DELETE_USER_STOCK:
       return {
+        ...state,
         success: true,
         items: state.items.filter(obj => obj.symbol !== action.symbol),
         symbol: action.symbol
